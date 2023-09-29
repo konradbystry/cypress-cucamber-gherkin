4e4812bb23e112b659478901d94f7634419d63c9
Konrad Bystrzykowski <k.bystrzykowski@testspring.pl>
2023-09-29 14:14:28 +0200

    added session catching for loggin command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,11 +1,13 @@
 import loginPage from "../fixtures/selectors/loginPage.json";
 
 Cypress.Commands.add("login", (username, password) => {
-  cy.visit(Cypress.env("loginPage"));
-  cy.get(loginPage.usernameInput)
-    .type(username)
-    .get(loginPage.passwordInput)
-    .type(password)
-    .get(loginPage.loginButton)
-    .click();
+  cy.session([username, password], () => {
+    cy.visit(Cypress.env("loginPage"));
+    cy.get(loginPage.usernameInput)
+      .type(username)
+      .get(loginPage.passwordInput)
+      .type(password)
+      .get(loginPage.loginButton)
+      .click();
+  });
 });
