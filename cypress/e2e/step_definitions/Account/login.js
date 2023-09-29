23bd9ceddc1a2ee1441b5cf90d23624dc254e604
Konrad Bystrzykowski <k.bystrzykowski@testspring.pl>
2023-09-29 14:48:21 +0200

    removed incorrect login data from step definitions

--- cypress/e2e/step_definitions/Account/login.js
+++ cypress/e2e/step_definitions/Account/login.js
@@ -4,8 +4,6 @@ import alerts from "../../../fixtures/selectors/alerts.json";
 
 const username = Cypress.env("username");
 const password = Cypress.env("password");
-const incorrectUsername = "incorrect_username";
-const incorrectPassword = "incorrect_password";
 
 Given("A user opens login page", () => {
   cy.visit(Cypress.env("loginPage"));
@@ -15,17 +13,23 @@ When("A user enter correct username into input", () => {
   cy.get(loginPage.usernameInput).type(username);
 });
 
-When("A user enter incorrect username into input", () => {
-  cy.get(loginPage.usernameInput).type(incorrectUsername);
-});
+When(
+  "A user enter incorrect username {string} into input",
+  (incorrectUsername) => {
+    cy.get(loginPage.usernameInput).type(incorrectUsername);
+  }
+);
 
 When("A user enter correct password into input", () => {
   cy.get(loginPage.passwordInput).type(password);
 });
 
-When("A user enter incorrect password into input", () => {
-  cy.get(loginPage.passwordInput).type(incorrectPassword);
-});
+When(
+  "A user enter incorrect password {string} into input",
+  (incorrectPassword) => {
+    cy.get(loginPage.passwordInput).type(incorrectPassword);
+  }
+);
 
 When("A user clicks on the login button", () => {
   cy.get(loginPage.loginButton).click();
