7d6dfa88c9afd32a6e7aaae5259e293cb998df88
Konrad Bystrzykowski <k.bystrzykowski@testspring.pl>
2023-10-04 14:19:59 +0200

    added command to login with user object as param

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,3 +11,15 @@ Cypress.Commands.add("login", (username, password) => {
       .click();
   });
 });
+
+Cypress.Commands.add("loginUser", ({ username, password }) => {
+  cy.session([username, password], () => {
+    cy.visit(Cypress.env("loginPage"));
+    cy.get(loginPage.usernameInput)
+      .type(username)
+      .get(loginPage.passwordInput)
+      .type(password)
+      .get(loginPage.loginButton)
+      .click();
+  });
+});
