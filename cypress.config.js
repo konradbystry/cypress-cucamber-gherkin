7f5de96029aef470beb2d104ebd3acf8175b6e34
Konrad Bystrzykowski <k.bystrzykowski@testspring.pl>
2023-10-04 14:18:50 +0200

    added account routes and created user object

--- cypress.config.js
+++ cypress.config.js
@@ -28,9 +28,20 @@ module.exports = defineConfig({
     setupNodeEvents,
   },
   env: {
-    username: process.env.USER_NAME,
-    password: process.env.PASSWORD,
+    user: {
+      username: process.env.USER_NAME,
+      password: process.env.PASSWORD,
+    },
     loginPage: process.env.LOGIN_PAGE,
     accountPage: process.env.ACCOUNT_PAGE,
+    editAccountDetailsPage: process.env.EDIT_ACCOUNT_DETAILS_PAGE,
+    changePasswordPage: process.env.CHANGE_PASSWORD_PAGE,
+    addressBookPage: process.env.ADDRESS_BOOK_PAGE,
+    wishlistPage: process.env.WISHLIST_PAGE,
+    orderHistoryPage: process.env.ORDER_HISTORY_PAGE,
+    transactionHistoryPage: process.env.TRANSACTION_HISTORY_PAGE,
+    downloadsPage: process.env.DOWNLOADS_PAGE,
+    notificationsPage: process.env.NOTIFICATIONS_PAGE,
+    logoffPage: process.env.LOGOFF_PAGE,
   },
 });
