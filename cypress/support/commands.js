1b4a0540cbe1a12c12084fbb57cec4b7b057398c
Konrad Bystrzykowski <k.bystrzykowski@testspring.pl>
2023-10-11 16:40:23 +0200

    addedd new address commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,5 @@
 import loginPage from "../fixtures/selectors/loginPage.json";
+import addressBookPage from "../fixtures/selectors/addressBookPage.json";
 
 Cypress.Commands.add("login", (username, password) => {
   cy.session([username, password], () => {
@@ -23,3 +24,44 @@ Cypress.Commands.add("loginUser", ({ username, password }) => {
       .click();
   });
 });
+
+Cypress.Commands.add(
+  "fillNewAddressForm",
+  (firstname, lastname, company, address1, city, region, zip, country) => {
+    cy.get(addressBookPage.addressForm.firstnameInput)
+      .type(firstname)
+      .get(addressBookPage.addressForm.lastnameInput)
+      .type(lastname)
+      .get(addressBookPage.addressForm.companyInput)
+      .type(company)
+      .get(addressBookPage.addressForm.address1Input)
+      .type(address1)
+      .get(addressBookPage.addressForm.cityInput)
+      .type(city)
+      .get(addressBookPage.addressForm.selectCountryInput)
+      .select(country)
+      .get(addressBookPage.addressForm.selectRegionInput)
+      .select(region)
+      .get(addressBookPage.addressForm.zipInput)
+      .type(zip);
+  }
+);
+
+Cypress.Commands.add(
+  "addNewAddress",
+  (firstname, lastname, company, address1, city, region, zip, country) => {
+    cy.visit(Cypress.env("addNewAddressPage"))
+      .fillNewAddressForm(
+        firstname,
+        lastname,
+        company,
+        address1,
+        city,
+        region,
+        zip,
+        country
+      )
+      .get(addressBookPage.addressForm.continueButton)
+      .click();
+  }
+);
